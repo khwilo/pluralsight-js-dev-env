774045187dca25335e2572062e42f56a59b49742
khwilo <khwilowatai@gmail.com>
2019-06-19 12:36:12 +0300

    Implement the logic to generate mock data

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,21 @@
+/*
+  Generate mock data for local development.
+  There is no need to point to an actual API.
+*/
+
+/* eslint-disable no-console */
+
+import jsf from 'json-schema-faker';
+import { schema } from './mockDataSchema';
+import fs from 'fs';
+import chalk from 'chalk';
+
+const json = JSON.stringify(jsf(schema));
+
+fs.writeFile("./src/api/db.json", json, err => {
+  if (err) {
+    return console.log(chalk.red(err));
+  } else {
+    console.log(chalk.green("Mock data generated."));
+  }
+});
